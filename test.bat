bba7bfafd96c041a8d7a5fd7b436aa5eeef89e92
Jesiel Faria <fariajesiel@gmail.com>
2023-08-29 13:27:20 -0300

    Salvando arquivo

---
+++ test.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem 1- Substitua pelo caminho do seu repositório
+cd /d "D:\Arquivos de Programas\Projetos\ark-save-smart-breeding" 
+
+rem 2 - Empurra para a branch o tudo que tiver alterado
+git add .
+git commit -m "Salvando arquivo"
+git push
+
+
+
+
+rem Coloca um comentário
+
+rem 3- Comando para nao fechar o terminal
+pause
