b18d7b45f11a0e834e3c850cabd1dbcae978daec
Jesiel Faria <fariajesiel@gmail.com>
2023-08-29 15:57:02 -0300

    Voltando arquivos executáveis

---
+++ 2-puxar-alteracao.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem 1- Substitua pelo caminho do seu repositório/projeto
+cd /d "D:\Arquivos de Programas\Projetos\ark-save-smart-breeding" 
+
+rem 2- Puxa todas as alterações
+git pull
+
+rem 3- Comando para não fechar o terminal
+pause
